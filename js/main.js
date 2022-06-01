9eee3ef6ab41212c18d41d1f647240d06480b93f
Martin Orefice <orefice9b@gmail.com>
2022-06-01 01:25:46 -0300

    Desafio complementario

--- js/main.js
+++ js/main.js
@@ -1,19 +1,5 @@
 //Desafio Complementario 01
 
-/*let ingreso=parseInt(prompt("Ingrese la cantidad de Holas"))
-
-for (let i = 0; i <= ingreso ; i++) {
-    
-    console.log(i);
-}
-*/
-/*let ingreso=parseInt(prompt("Ingresá un número. \n Para salir ingresa ESC"))
-
-while (ingreso != "ESC") {
-    alert("El ususario ingreso" + ingreso);
-    ingreso=prompt("Ingresá otro número. \n Para salir ingresa ESC")
-}*/
-
 
 //BUCLE DE HOLAS
 let entrada = parseInt(prompt("Numero de HOLAS"));
