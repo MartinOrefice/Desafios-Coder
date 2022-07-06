b17b2719fee54489f32ae66047bc37547ab80e1f
Martin Orefice <orefice9b@gmail.com>
2022-07-05 23:25:33 -0300

    Optimizando proyecto

--- js/inicio.js
+++ js/inicio.js
@@ -5,6 +5,28 @@ const btnIniciar = document.querySelector("#iniciar");
 
 
 btnIniciar.addEventListener("click", ()=>{
-  localStorage.setItem("Usuario", inputNombre.value);
-  window.location.href = "../pages/calculadora.html";
+  if (inputNombre.value == "") {
+    Toastify({
+      text: "Usuario no ingresado",
+      duration: 3000,
+      newWindow: true,
+      close: true,
+      gravity: "top", 
+      position: "right", 
+      stopOnFocus: true, 
+      style: {
+        background: "linear-gradient(to right, #d70e0e, #4e0d0d )",
+      },
+      onClick: function(){}
+    }).showToast();
+  } else {
+    localStorage.setItem("Usuario", inputNombre.value);
+    window.location.href = "../pages/calculadora.html";
+  }
 });
+
+
+
+
+
+
